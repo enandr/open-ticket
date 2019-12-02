c5cce0462eefaf8c9f371d77a5e47c3457c42e34
ziyaadm <ziyaadm@gmail.com>
2019-12-02 20:46:50 +0000

    seperated header and footer into components

--- client/components/myProjectList.jsx
+++ client/components/myProjectList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import MyProject from './myProject';
 
 export default class MyProjectList extends React.Component {
   constructor(props) {
@@ -19,81 +20,9 @@ export default class MyProjectList extends React.Component {
   render() {
     return (
       <table className="table">
-        <nav className="navbar navbar-expand-lg color fixed-top">
-          <a className="navbar-brand">Projects</a>
-        </nav>
         <tbody>
-          <tr>
-            <td><strong>Project 1</strong>
-              <p>
-                <small>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus.</small>
-              </p>
-            </td>
-          </tr>
-          <tr>
-            <td><strong>Project 2</strong>
-              <p>
-                <small>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus.</small>
-              </p>
-            </td>
-          </tr>
-          <tr>
-            <td><strong>Project 3</strong>
-              <p>
-                <small>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus.</small>
-              </p>
-            </td>
-          </tr>
-          <tr>
-            <td><strong>Project 4</strong>
-              <p>
-                <small>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus.</small>
-              </p>
-            </td>
-          </tr>
-          <tr>
-            <td><strong>Project 5</strong>
-              <p>
-                <small>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus.</small>
-              </p>
-            </td>
-          </tr>
-          <tr>
-            <td><strong>Project 6</strong>
-              <p>
-                <small>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus.</small>
-              </p>
-            </td>
-          </tr>
-          <tr>
-            <td><strong>Project 7</strong>
-              <p>
-                <small>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus.</small>
-              </p>
-            </td>
-          </tr>
-          <tr>
-            <td><strong>Project 8</strong>
-              <p>
-                <small>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus.</small>
-              </p>
-            </td>
-          </tr>
-          <tr>
-            <td><strong>Project 9</strong>
-              <p>
-                <small>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus.</small>
-              </p>
-            </td>
-          </tr>
+          <MyProject setView={this.props.setView}/>
         </tbody>
-        <footer className="footer">
-          <div className="row">
-            <button className="col">My Tickets</button>
-            <button className="col">Create</button>
-            <button className="col">All Tickets</button>
-          </div>
-        </footer>
       </table>
       // <div className="container">
       //   <div className="row">
---
+++ client/components/myProjectListFooter.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function MyProjectListNav(props) {
+  return (
+    <footer className="footer">
+      <div className="row">
+        <button className="col">My Tickets</button>
+        <button className="col">Create</button>
+        <button className="col">All Tickets</button>
+      </div>
+    </footer>
+  );
+}
