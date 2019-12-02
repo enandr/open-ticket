47751bf02da7e7b73c3e3e93b8874ad01672bc34
ziyaadm <ziyaadm@gmail.com>
2019-12-02 20:26:44 +0000

    static display of projects along with fixed navbar and footer

--- client/components/myProjectList.jsx
+++ client/components/myProjectList.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import MyProject from './myProject';
 
 export default class MyProjectList extends React.Component {
   constructor(props) {
@@ -19,17 +18,94 @@ export default class MyProjectList extends React.Component {
 
   render() {
     return (
-      <div className="container">
-        <div className="row">
-          <MyProject setView={this.props.setView} />
-          <button onClick={() => this.props.setView('myProjectList')}>
-            My Projects
-          </button>
-          <button onClick={() => this.props.setView('teamProjectList')}>
-            Team Projects
-          </button>
-        </div>
-      </div>
+      <table className="table">
+        <nav className="navbar navbar-expand-lg color fixed-top">
+          <a className="navbar-brand">Projects</a>
+        </nav>
+        <tbody>
+          <tr>
+            <td><strong>Project 1</strong>
+              <p>
+                <small>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus.</small>
+              </p>
+            </td>
+          </tr>
+          <tr>
+            <td><strong>Project 2</strong>
+              <p>
+                <small>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus.</small>
+              </p>
+            </td>
+          </tr>
+          <tr>
+            <td><strong>Project 3</strong>
+              <p>
+                <small>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus.</small>
+              </p>
+            </td>
+          </tr>
+          <tr>
+            <td><strong>Project 4</strong>
+              <p>
+                <small>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus.</small>
+              </p>
+            </td>
+          </tr>
+          <tr>
+            <td><strong>Project 5</strong>
+              <p>
+                <small>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus.</small>
+              </p>
+            </td>
+          </tr>
+          <tr>
+            <td><strong>Project 6</strong>
+              <p>
+                <small>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus.</small>
+              </p>
+            </td>
+          </tr>
+          <tr>
+            <td><strong>Project 7</strong>
+              <p>
+                <small>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus.</small>
+              </p>
+            </td>
+          </tr>
+          <tr>
+            <td><strong>Project 8</strong>
+              <p>
+                <small>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus.</small>
+              </p>
+            </td>
+          </tr>
+          <tr>
+            <td><strong>Project 9</strong>
+              <p>
+                <small>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus.</small>
+              </p>
+            </td>
+          </tr>
+        </tbody>
+        <footer className="footer">
+          <div className="row">
+            <button className="col">My Tickets</button>
+            <button className="col">Create</button>
+            <button className="col">All Tickets</button>
+          </div>
+        </footer>
+      </table>
+      // <div className="container">
+      //   <div className="row">
+      //     <MyProject setView={this.props.setView} />
+      //     <button onClick={() => this.props.setView('myProjectList')}>
+      //       My Projects
+      //     </button>
+      //     <button onClick={() => this.props.setView('teamProjectList')}>
+      //       Team Projects
+      //     </button>
+      //   </div>
+      // </div>
     );
   }
 }
