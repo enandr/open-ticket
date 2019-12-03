3c2bd8ab579e0edf55f6ad98f5377a41d8e13c6a
Jake Hassler <jphyr4@mail.missouri.edu>
2019-12-03 19:14:31 +0000

    added featherIcons for main page.

--- client/components/myProjectListFooter.jsx
+++ client/components/myProjectListFooter.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import CheckIcon from './CheckIcon';
+import PlusCircleIcon from './PlusCircleIcon';
+import GridIcon from './GridIcon';
 
 export default function MyProjectListNav(props) {
   return (
     <footer className="footer">
-      <div className="row">
-        <button className="col">My Tickets</button>
-        <button className="col">Create</button>
-        <button className="col">All Tickets</button>
+      <div className="row h-100">
+        <div className="col text-center">
+          <CheckIcon />
+          <p>My Tasks</p>
+        </div>
+        <div className="col text-center">
+          <PlusCircleIcon />
+          <p>Create</p>
+        </div>
+        <div className="col text-center">
+          <GridIcon />
+          <p>Board</p>
+        </div>
       </div>
     </footer>
   );
