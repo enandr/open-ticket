e6fec3ea190f26586090580a42858bfeeb8afe46
ziyaadm <ziyaadm@gmail.com>
2019-12-06 01:40:18 +0000

    bootstrap for team ticket view

--- client/components/teamTicket.jsx
+++ client/components/teamTicket.jsx
@@ -10,26 +10,37 @@ export default function TeamTicket(props) {
     }}>
       <td>
         <strong>{props.value.ticketTitle}</strong>
-        <p>
-          <small>{props.value.statusCode}</small>
-        </p>
-        <p>
-          <small>{props.value.ticketType}</small>
-        </p>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm">
+              <small>Status: {props.value.statusCode}</small>
+            </div>
+            <div className="col-sm">
+              <small>Type: {props.value.ticketType}</small>
+            </div>
 
-        <p>
-          <small>{props.value.assigneeName}</small>
-        </p>
-        <p>
-          <small>{props.value.priorityLevel}</small>
+          </div>
 
-        </p>
-        <p>
-          <small>Due Date: {props.value.dueDate}</small>
-        </p>
-        <p>
-          <small>Created At: {props.value.createdAt}</small>
-        </p>
+          <div className="row">
+            <div className="col-sm">
+              <small>Assignee: {props.value.assigneeName}</small>
+            </div>
+            <div className="col-sm">
+              <small>Priority: {props.value.priorityLevel}</small>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-sm">
+              <small>Due Date: {props.value.dueDate}</small>
+            </div>
+            <div className="col-sm">
+              <small>Created At: {props.value.createdAt}</small>
+            </div>
+          </div>
+
+        </div>
+
       </td>
     </tr>
   );
