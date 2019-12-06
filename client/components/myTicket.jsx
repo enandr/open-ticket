0bf647b16d3f54d9bcbcaecee0a471c59e7466b1
Jake Hassler <jphyr4@mail.missouri.edu>
2019-12-06 20:53:05 +0000

    font titles 20 px subtitles 17px. centered project table data

--- client/components/myTicket.jsx
+++ client/components/myTicket.jsx
@@ -11,41 +11,43 @@ export default function MyTicket(props) {
 
     }}>
       <td>
-        <strong>{props.value.ticketTitle}</strong>
+        <strong className="ticketTitle">{props.value.ticketTitle}</strong>
         <div className="container">
           <div className="row">
             <div className="col-sm">
-              <small>Status: {props.value.statusCode}</small>
+              <small className="ticketInfo">Status: {props.value.statusCode}</small>
             </div>
             <div className="col-sm">
-              <small>Type: {props.value.ticketType}</small>
+              <small className="ticketInfo">Type: {props.value.ticketType}</small>
             </div>
 
           </div>
 
           <div className="row">
             <div className="col-sm">
-              <small>Assignee: {props.value.assigneeName}</small>
+              <small className="ticketInfo">Assignee: {props.value.assigneeName}</small>
             </div>
             <div className="col-sm">
-              <small>Priority: {props.value.priorityLevel}</small>
+              <small className="ticketInfo">Priority: {props.value.priorityLevel}</small>
             </div>
           </div>
 
           <div className="row">
             <div className="col-sm">
 
-              <small>Due Date: {props.value.dueDate}</small>
+              <small className="ticketInfo">Due Date: {props.value.dueDate}</small>
 
             </div>
             <div className="col-sm">
-              <small>Created At: {props.value.createdAt}</small>
+              <small className="ticketInfo">Created At: {props.value.createdAt}</small>
             </div>
           </div>
 
         </div>
-        <Circle/>
-        <Chevron/>
+        <div className="">
+          <Circle/>
+          <Chevron/>
+        </div>
       </td>
     </tr>
   );
