6762914e2543965dcf83e1bbb85122a1a89578de
Jake Hassler <jphyr4@mail.missouri.edu>
2019-12-06 18:24:53 +0000

    fixed navbar and centerd icons

--- client/components/myProjectListFooter.jsx
+++ client/components/myProjectListFooter.jsx
@@ -5,19 +5,19 @@ import GridIcon from './GridIcon';
 
 export default function MyProjectListFooter(props) {
   return (
-    <footer className="footer">
-      <div className="row h-100 d-flex align-items-center">
-        <div className="col text-center clickable" onClick={() => props.setView('myProjectList')}>
+    <footer className="footer d-flex justify-content-around">
+      <div className="row navbar">
+        <div className="col-4 text-center" onClick={() => props.setView('myProjectList')}>
           <CheckIcon/>
-          <p>My Tasks</p>
+          <p className="navText">My Tasks</p>
         </div>
-        <div className="col text-center clickable" onClick={() => props.setView('create', 'myProjectList')}>
+        <div className="col-4 text-center" onClick={() => props.setView('create', 'myProjectList')}>
           <PlusCircleIcon/>
-          <p>Create</p>
+          <p className="navText">Create</p>
         </div>
-        <div className="col text-center clickable" onClick={() => props.setView('teamProjectList')}>
+        <div className="col-4 text-center" onClick={() => props.setView('teamProjectList')}>
           <GridIcon/>
-          <p>Board</p>
+          <p className="navText">Board</p>
         </div>
       </div>
     </footer>
