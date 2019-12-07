b816f65017e8cf596e735c25464e2f32d7db24c3
khoacao0601 <khoacao0601@gmail.com>
2019-12-07 12:01:07 -0800

    Design Log In Page

--- client/components/login.jsx
+++ client/components/login.jsx
@@ -48,21 +48,44 @@ export default class Login extends React.Component {
         <div className="appName">
           <h1>OpenTicket</h1>
         </div>
-        <div className="logInContainer">
-          <form onSubmit={this.handleSubmit}>
-            <p>{this.state.status}</p>
-            <div className="username">
-              <label>Username: </label>
-              <input type="text" className="form-control" placeholder="Name" value={this.state.name} onChange={this.handleChangeUserName}></input>
-            </div>
-            <div className="password">
-              <label>Password: </label>
-              <input type="password" className="form-control" placeholder="Password" value={this.state.password} onChange={this.handleChangePass}></input>
-            </div>
-            <p onClick={this.handleChangeView}>Create Account ?</p>
-            <button className="btn btn-success" type="submit">Login</button>
-          </form>
+        <div className="card">
+          <article className="card-body">
+            <a href className="float-right btn btn-outline-primary" onClick={this.handleChangeView}>Sign up</a>
+            <h4 className="card-title mb-4 mt-1">Sign in</h4>
+            <form onSubmit={this.handleSubmit}>
+              <div className="form-group">
+                <label>Username:</label>
+                <input name className="form-control" placeholder="Username" type="text" value={this.state.name} onChange={this.handleChangeUserName}/>
+              </div>
+              <div className="form-group">
+                <label>Your password</label>
+                <input className="form-control" placeholder="******" type="password" value={this.state.password} onChange={this.handleChangePass}/>
+              </div>
+              <div className="form-group">
+                <button type="submit" className="btn btn-primary btn-block"> Login</button>
+              </div>
+            </form>
+          </article>
         </div>
+
+        {/* <div>
+
+          <div className="logInContainer">
+            <form onSubmit={this.handleSubmit}>
+              <p>{this.state.status}</p>
+              <div className="username">
+                <label>Username: </label>
+                <input type="text" className="form-control" placeholder="Name" value={this.state.name} onChange={this.handleChangeUserName}></input>
+              </div>
+              <div className="password">
+                <label>Password: </label>
+                <input type="password" className="form-control" placeholder="Password" value={this.state.password} onChange={this.handleChangePass}></input>
+              </div>
+              <p onClick={this.handleChangeView}>Create Account ?</p>
+              <button className="btn btn-success" type="submit">Login</button>
+            </form>
+          </div>
+        </div> */}
       </div>
     );
   }
