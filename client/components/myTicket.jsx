8b2ab0fc84f886b4f40cbdc7ce6e9790644a4a4d
Jake Hassler <jphyr4@mail.missouri.edu>
2019-12-09 22:43:10 +0000

    cleaned up front end white spaces, set navbar title font size

--- client/components/myTicket.jsx
+++ client/components/myTicket.jsx
@@ -14,44 +14,43 @@ export default function MyTicket(props) {
 
         <div className="container">
           <div className="">
-            <div className="">
-              <strong className="ticketTitle">{props.value.ticketTitle}</strong>
-              <div className="row">
 
-                <div className="col-sm">
-                  <small className="ticketInfo">Status: {props.value.statusCode}</small>
-                </div>
-                <div className="col-sm">
-                  <small className="ticketInfo">Type: {props.value.ticketType}</small>
-                </div>
+            <strong className="ticketTitle">{props.value.ticketTitle}</strong>
+            <div className="row">
 
+              <div className="col-sm">
+                <small className="ticketInfo">Status: {props.value.statusCode}</small>
               </div>
-
-              <div className="row">
-                <div className="col-sm">
-                  <small className="ticketInfo">Assignee: {props.value.assigneeName}</small>
-                </div>
-                <div className="col-sm">
-                  <small className="ticketInfo">Priority: {props.value.priorityLevel}</small>
-                </div>
+              <div className="col-sm">
+                <small className="ticketInfo">Type: {props.value.ticketType}</small>
               </div>
-
-              <div className="row">
-                <div className="col-sm">
-
-                  <small className="ticketInfo">Due Date: {props.value.dueDate}</small>
-
-                </div>
-                <div className="col-sm">
-                  <small className="ticketInfo">Created At: {props.value.createdAt.slice(0, 10)}</small>
-                </div>
-              </div>
-            </div>
-            <div className="">
-              <Circle fill={props.value.statusCode}/>
-              <Chevron />
 
             </div>
+
+            <div className="row">
+              <div className="col-sm">
+                <small className="ticketInfo">Assignee: {props.value.assigneeName}</small>
+              </div>
+              <div className="col-sm">
+                <small className="ticketInfo">Priority: {props.value.priorityLevel}</small>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-sm">
+
+                <small className="ticketInfo">Due Date: {props.value.dueDate}</small>
+
+              </div>
+              <div className="col-sm">
+                <small className="ticketInfo">Created At: {props.value.createdAt.slice(0, 10)}</small>
+              </div>
+            </div>
+          </div>
+          <div className="">
+            <Circle fill={props.value.statusCode}/>
+            <Chevron />
+
           </div>
         </div>
 
