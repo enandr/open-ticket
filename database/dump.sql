ce588e4fc48bb45f16314367ab907dafcaf735c1
Jake Hassler <jphyr4@mail.missouri.edu>
2019-12-10 00:46:06 +0000

    dummy data

--- database/dump.sql
+++ database/dump.sql
@@ -27,7 +27,7 @@ CREATE TABLE `files` (
   `ticketId` int(11) NOT NULL,
   `fileUrl` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -36,6 +36,7 @@ CREATE TABLE `files` (
 
 LOCK TABLES `files` WRITE;
 /*!40000 ALTER TABLE `files` DISABLE KEYS */;
+INSERT INTO `files` VALUES (1,1,'/images/.'),(2,2,'/images/.'),(3,3,'/images/.'),(4,4,'/images/.'),(5,5,'/images/.'),(6,6,'/images/.');
 /*!40000 ALTER TABLE `files` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -77,7 +78,7 @@ CREATE TABLE `projects` (
   `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `createdBy` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -86,7 +87,7 @@ CREATE TABLE `projects` (
 
 LOCK TABLES `projects` WRITE;
 /*!40000 ALTER TABLE `projects` DISABLE KEYS */;
-INSERT INTO `projects` VALUES (1,'Student Grade Table','An app to display a list of students courses, and their grades','2019-12-09 23:50:48',1),(2,'Star Wars Memory Match','A card memory match game with a Star Wars Theme','2019-12-09 23:52:18',1),(3,'Stay In Go Out','SI/GO is an app that, based on the weather, recommends indoor or outdoor activities.','2019-12-09 23:55:48',1),(4,'Terraforming Mars','A digitalized board game where you terraform mars to make it livable','2019-12-09 23:57:32',1),(5,'Events Near You','A simple project implementing EventBrite API to find and displays events nearby the user.','2019-12-09 23:58:32',3),(6,'Xbox GamePass','Xbox Game Pass is a video game subscription service for use with the Xbox One console and Windows 10. ','2019-12-09 23:59:44',4),(7,'Overwatch Memory Match','Match your favorite Overwatch characters in this game based on Blizzard’s hit first person shooter.','2019-12-10 00:00:13',4),(8,'Gemstone Mining','A re-creation of the Disney Gemstone Mining game written in Javascript OOP.','2019-12-10 00:00:49',3),(10,'Balance Health','Tracking what you did what you eat and how long you sleep to give you the recommendation.','2019-12-10 00:01:55',2),(11,'Travel buddy','Let you know the weather in the area and the top 5 restaurant in that area.','2019-12-10 00:02:28',2),(12,'openTicket','A React based issue and feature tracking system for web development projects. Built for developers by developers. word word.','2019-12-10 00:07:19',4);
+INSERT INTO `projects` VALUES (1,'Student Grade Table','An app to display a list of students courses, and their grades','2019-12-09 23:50:48',1),(2,'Star Wars Memory Match','A card memory match game with a Star Wars Theme','2019-12-09 23:52:18',1),(3,'Stay In Go Out','SI/GO is an app that, based on the weather, recommends indoor or outdoor activities.','2019-12-09 23:55:48',1),(4,'Terraforming Mars','A digitalized board game where you terraform mars to make it livable','2019-12-09 23:57:32',1),(5,'Events Near You','A simple project implementing EventBrite API to find and displays events nearby the user.','2019-12-09 23:58:32',3),(6,'Xbox GamePass','Xbox Game Pass is a video game subscription service for use with the Xbox One console and Windows 10. ','2019-12-09 23:59:44',4),(7,'Overwatch Memory Match','Match your favorite Overwatch characters in this game based on Blizzard’s hit first person shooter.','2019-12-10 00:00:13',4),(8,'Gemstone Mining','A re-creation of the Disney Gemstone Mining game written in Javascript OOP.','2019-12-10 00:00:49',3),(10,'Balance Health','Tracking what you did what you eat and how long you sleep to give you the recommendation.','2019-12-10 00:01:55',2),(11,'Travel buddy','Let you know the weather in the area and the top 5 restaurant in that area.','2019-12-10 00:02:28',2),(12,'openTicket','A React based issue and feature tracking system for web development projects. Built for developers by developers. word word.','2019-12-10 00:07:19',4),(15,'Memory Match','A themed, javascript memory match game that tracks player statistics.','2019-12-10 00:26:23',3);
 /*!40000 ALTER TABLE `projects` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -135,7 +136,7 @@ CREATE TABLE `tickets` (
   `createdBy` int(11) NOT NULL,
   `updateAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -144,6 +145,7 @@ CREATE TABLE `tickets` (
 
 LOCK TABLES `tickets` WRITE;
 /*!40000 ALTER TABLE `tickets` DISABLE KEYS */;
+INSERT INTO `tickets` VALUES (1,'Create Cards Dynamically','Allow the cards to be created dynamically using jQuery.','2019-12-09',15,4,2,2,3,'2019-12-10 00:32:16',3,'2019-12-10 00:32:16'),(2,'Randomize Cards','Take the array of cards and randomize them at the start of each match.','2019-12-21',15,3,1,2,3,'2019-12-10 00:35:32',3,'2019-12-10 00:35:32'),(3,'Finish MVP','Finish the basic functionality of the application. Statistics, cards, game board, etc.','2019-12-07',15,2,3,2,3,'2019-12-10 00:37:23',3,'2019-12-10 00:37:23'),(4,'Basic CSS - Front End','Complete the basic styling of the app, including a theme.','2019-12-07',15,3,3,2,3,'2019-12-10 00:38:30',3,'2019-12-10 00:38:30'),(5,'Add Card Flip Transitions','Allow the cards to flip over, revealing the card face.','2019-12-11',15,4,2,1,3,'2019-12-10 00:39:34',3,'2019-12-10 00:39:34'),(6,'Clicking Same Card Twice','Selecting the same card twice will count as a match within the game.','2019-12-20',15,4,1,1,3,'2019-12-10 00:42:11',3,'2019-12-10 00:42:11');
 /*!40000 ALTER TABLE `tickets` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -183,7 +185,7 @@ CREATE TABLE `userProjects` (
   `projectId` int(11) NOT NULL,
   `userId` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=42 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -192,7 +194,7 @@ CREATE TABLE `userProjects` (
 
 LOCK TABLES `userProjects` WRITE;
 /*!40000 ALTER TABLE `userProjects` DISABLE KEYS */;
-INSERT INTO `userProjects` VALUES (1,1,1),(2,2,1),(3,3,1),(4,3,2),(5,3,3),(6,3,4),(7,4,1),(8,4,3),(9,4,2),(10,4,4),(11,5,3),(12,5,1),(13,5,2),(14,5,4),(15,6,4),(16,6,1),(17,6,2),(18,6,3),(19,7,4),(20,8,3),(21,8,2),(22,8,1),(23,8,4),(26,9,1),(27,9,4),(28,10,2),(29,11,2),(30,12,4),(31,12,2),(32,12,1),(33,12,3);
+INSERT INTO `userProjects` VALUES (1,1,1),(2,2,1),(3,3,1),(4,3,2),(5,3,3),(6,3,4),(7,4,1),(8,4,3),(9,4,2),(10,4,4),(11,5,3),(12,5,1),(13,5,2),(14,5,4),(15,6,4),(16,6,1),(17,6,2),(18,6,3),(19,7,4),(20,8,3),(21,8,2),(22,8,1),(23,8,4),(26,9,1),(27,9,4),(28,10,2),(29,11,2),(30,12,4),(31,12,2),(32,12,1),(33,12,3),(39,15,3);
 /*!40000 ALTER TABLE `userProjects` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -232,4 +234,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-12-10  0:17:13
+-- Dump completed on 2019-12-10  0:45:53
